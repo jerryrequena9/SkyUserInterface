183892b7ece5b87290729639534c3cbe5427df05
Jerry Requena <colt2@ymail.com>
2025-04-23 14:28:58 -0700

    foruth commit

--- SkyUserInterface/src/navigationView.jsx
+++ SkyUserInterface/src/navigationView.jsx
@@ -4,6 +4,7 @@ import React from "react";
 function NavMenu(){
     return(
         <div id="menuContainer">
+            <div id="PageLogo"><p>Raiser's Edge</p></div>
             <div className="NavMenuLinks">Home Page</div>
             <div className="NavMenuLinks">Dashboard</div>
             <div className="NavMenuLinks">Non Profit Search</div>
