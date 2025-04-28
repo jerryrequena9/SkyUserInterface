b0b8ef9af04dc2b09356ddfe615db18fe333f145
Jerry Requena <colt2@ymail.com>
2025-04-28 16:34:38 -0700

    sixth commit

--- SkyUserInterface/src/LoginForm.jsx
+++ SkyUserInterface/src/LoginForm.jsx
@@ -1,8 +1,50 @@
 import "./LoginForm.css"
+import { useState } from "react";
+
+// fetch(url, options)
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.error('Error:', error));
+function LoginForm(){ 
+const [UserEmail,setEmail]=useState("");
+const [UserPassword,setPassword]=useState("");
 
 
 
-function LoginForm(){
+
+const handleEmailChange=(newEmail)=>{
+    console.log(newEmail);
+    setEmail(newEmail);
+}
+
+const handlePasswordChange=(newPassword)=>{
+    console.log(newPassword);
+    setPassword(newPassword);
+}
+
+
+const handleSubmit=()=>{
+console.log(UserEmail);
+console.log(UserPassword);
+let url="https://680fd18827f2fdac240f7ceb.mockapi.io/login?email="+UserEmail+"&"+"password="+UserPassword;
+let options= {method: 'GET',headers: {'content-type':'application/json'},};
+if(UserEmail!='' && UserPassword!=''){
+    fetch(url,options)
+    .then(res =>{if (res.ok) {return res.json();}})
+      .then(data =>{
+        console.log(data);
+      })
+      .catch(error => {
+    // handle error
+  })
+}
+else{
+    window.alert('User email or password are incorrect.')
+}
+}
+
+
+
 
 
     return(
@@ -11,8 +53,10 @@ function LoginForm(){
             <div id="LoginForm">
             <img src="\assests\login_40dp_FFFFFF_FILL0_wght400_GRAD0_opsz40.png" alt="Login icon" />
             <h2>Sign In</h2>
-            <label>Username: </label><input id="userNameField"></input><br/><br/>
-            <label>Password:  </label><input id="userPasswordField"></input>
+            <label>Username: </label><input onChange={(event)=>handleEmailChange(event.target.value)} type="email" id="userNameField" required/><br/><br/>
+            <label>Password:  </label><input onChange={(event)=>handlePasswordChange(event.target.value)} type="password" id="userPasswordField" required/><br/><br/>
+            <button onClick={handleSubmit} id="UserLoginBtn">Login</button>
+            <button id="UserCancelnBtn">Cancel</button>
             </div>
         </div>
     );
